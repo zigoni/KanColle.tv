c41d24ab5f4a38e520745fd651cc555a9b289de2
xchange <xchange@live.com>
2014-12-20 21:32:15 +0800

    用户部分的views，以及url采用reverse方法生成

--- kc/kc/urls.py
+++ kc/kc/urls.py
@@ -8,9 +8,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'kc_home.views.home'),
-    url(r'^connector/$', 'kc_connector.views.home'),
-    url(r'^proxy/$', 'kc_home.views.proxy'),
-    url(r'^hosts/$', 'kc_home.views.hosts'),
+    url(r'^$', 'kc_home.views.home', name='kc-home'),
+    url(r'^connector/$', 'kc_connector.views.home', name='kc-connector'),
+    url(r'^proxy/$', 'kc_home.views.proxy', name='kc-proxy'),
+    url(r'^hosts/$', 'kc_home.views.hosts', name='kc-hosts'),
     url(r'^user/', include('kc_user.urls')),
 )
---
+++ kc/kc_user/views.py
@@ -0,0 +1,66 @@
+import hashlib
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from kc_user.config import *
+from kc_user.models import *
+from kc_user.forms import *
+
+
+context = {'active': 'user'}
+
+
+def signup(request):
+    if KC_USER_SIGNUP:
+        if request.user.is_authenticated():
+            context['title'] = '已注册用户不能重复注册'
+            context['message'] = '您已经是KanColle.tv的注册用户了，无需重新注册！'
+            return render(request, 'warning.html', context)
+        form = SignupForm(request.POST or None)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = KcUser.objects.create_user(email=email, username=username, password=password)
+            user.save()
+
+            hashstr = email+username+password+KcUser.objects.make_random_password()
+            email_confirmation_code = hashlib.md5(hashstr.encode('utf-8')).hexdigest()
+            email_confirmation = KcUserEmailConfirmation.objects.create(user=user, code=email_confirmation_code)
+            email_confirmation.save()
+
+            mail_subject = 'KanColle.tv注册用户激活'
+            mail_body = '%s，您好！\n感谢您注册成为KanColle.tv的用户，请用下面的链接激活您的账号：\n\nhttp://kancolle.tv/user/confirmation/%s/\n\n本邮件由系统自动发送，请勿回复' % (username, email_confirmation_code)
+            mail_from = 'webmaster@kancolle.tv'
+            mail_to = [email, ]
+            send_mail(mail_subject, mail_body, mail_from, mail_to)
+
+            context['title'] = '注册成功'
+            context['message'] = '注册KanColle.tv用户成功，请查收您的邮箱以激活您的账号。'
+            return render(request, 'message.html', context)
+        context['form'] = form
+        return render(request, 'kc_user/signup.html', context)
+    else:
+        context['title'] = '关闭注册'
+        context['message'] = '本站暂不开放注册。'
+        return render(request, 'message.html', context)
+
+
+def confirmation(request, code):
+    try:
+        confirmation = KcUserEmailConfirmation.objects.get(code=code)
+    except KcUserEmailConfirmation.DoesNotExist:
+        context['title'] = '激活码无效'
+        context['message'] = '激活码无效，激活失败！'
+        return render(request, 'warning.html', context)
+    user = confirmation.user
+    user.is_active = True
+    user.save(update_fields=['is_active'])
+    confirmation.delete()
+    context['title'] = '激活成功'
+    context['message'] = '您的账号已激活成功，请从页面右上角的登录按钮登录。'
+    return render(request, 'message.html', context)
+
+
+def forgetpassword(request):
+    pass
