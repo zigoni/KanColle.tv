2080479cde97ba0155eda8fe0ab66b530b59807a
xchange <xchange@live.com>
2014-12-21 09:39:06 +0800

    用户管理界面增加一个搜索框

--- kc/kc_user/admin.py
+++ kc/kc_user/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from kc_user.models import KcGroup, KcUser
 
 
+class KcUserAdmin(admin.ModelAdmin):
+    search_fields = ['email', 'username']
+
+
 admin.site.register(KcGroup)
-admin.site.register(KcUser)
+admin.site.register(KcUser, KcUserAdmin)
