6756420e14d1ae7f2c66658e7d83cd6b8bb10fc6
xchange <xchange@live.com>
2015-01-06 10:01:14 +0800

    Debug step 3

--- kc/kc_doujin/forms.py
+++ kc/kc_doujin/forms.py
@@ -36,7 +36,7 @@ class KcUploadedComicFileEditForm(forms.Form):
     def clean_file_name(self):
         file_name = self.cleaned_data['file_name']
         path = os.path.join(KC_DOUJIN_UPLOAD_DIR, file_name)
-        base_dir = os.path.dirname(KC_DOUJIN_UPLOAD_DIR)
+        base_dir = os.path.abspath(KC_DOUJIN_UPLOAD_DIR)
         upload_dir = os.path.dirname(os.path.abspath(path))
         logger.debug('%s, %s, %s' % (path, base_dir, upload_dir))
         if base_dir != upload_dir:
